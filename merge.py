9d58b0e546eb18c3de3faab0a7c97c3b2af3a538
kmij.str5_homepc <kmij.str5@gmail.com>
2024-03-15 00:41:20 +0900

    format

--- merge.py
+++ merge.py
@@ -4,7 +4,7 @@ import einops
 import numpy as np
 
 
-def eachFile(path_file):
+def eachFile(path_file) -> list:
     """
     get file name from path_file
     :param path_file: target path
@@ -17,7 +17,7 @@ def eachFile(path_file):
     return fileName
 
 
-def slice_rank(slices):
+def slice_rank(slices) -> int:
     try:
         start_index = slices.find(".tif") + len(".tif")
     except:
@@ -32,22 +32,22 @@ def slice_rank(slices):
 
 
 # 定义2D TIFF切片的文件名列表
-tiff_slices = eachFile("data")
-sort_slices = sorted(tiff_slices, key=slice_rank)
+tiff_slices: list = eachFile("data")
+sort_slices: list = sorted(tiff_slices, key=slice_rank)
 print(sort_slices)
 
 # 读取所有切片并存储在一个列表中
-images = [tifffile.imread("data/" + slice_file) for slice_file in sort_slices]
+images: list = [tifffile.imread("data/" + slice_file) for slice_file in sort_slices]
 
-image_crop = [element[107:363, 191:447, 1] for element in images]
+image_crop:list = [element[107:363, 191:447, 1] for element in images]
 
 # 将列表中的图像堆叠成一个3D数组
-image_stack = np.stack(image_crop, axis=0)
+image_stack: np.ndarray = np.stack(image_crop, axis=0)
 
 # image_stack = einops.rearrange(image_stack, "z y x -> y z x")
 
-image_np  = image_stack[50]
-image_np_transformed = np.zeros_like(image_np)
+image_np: np.ndarray = image_stack[50]
+image_np_transformed: np.ndarray = np.zeros_like(image_np)
 image_np_transformed[(image_np >= 0) & (image_np < 128)] = 0
 image_np_transformed[(image_np >= 128) & (image_np < 192)] = 128
 image_np_transformed[(image_np >= 192) & (image_np <= 255)] = 255
