2653e77d78245421a88877ad2e5be410b801c6c8
kmij.str_macair <kmij.str5@gmail.com>
2024-07-02 20:23:18 +0900

    Add function: 3D cube

--- cube3D.py
+++ cube3D.py
@@ -1,37 +1,17 @@
+import tifffile as tiff
+from mayavi import mlab
 import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 
-# 创建一个 100x100x100 的 3D 矩阵，包含 3 种不同的值
-np.random.seed(0)
-matrix = np.random.choice([0, 1, 2], size=(100, 100, 100))
+# 读取图像
+image = tiff.imread('2phase.tif')
+data = image[:50, :50, :50]
 
-# 创建一个颜色映射
-colors = np.empty(matrix.shape, dtype=object)
-colors[matrix == 0] = 'r'
-colors[matrix == 1] = 'g'
-colors[matrix == 2] = 'b'
+# 创建Mayavi中的标量场
+src = mlab.pipeline.scalar_field(data)
 
-# 设置旋转的角度
-angles = np.linspace(0, 360, 36)
+# 为两个不同的值创建等值面，分别设置不同的颜色
+mlab.pipeline.iso_surface(src, contours=[1], color=(1, 0, 0), opacity=0.15)  # 红色
+mlab.pipeline.iso_surface(src, contours=[0], color=(0, 1, 0), opacity=0.15)  # 绿色
 
-# 创建一个 3D 图形
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-# 绘制 3D 矩阵中的每个点
-def plot_matrix(ax, matrix, colors):
-    ax.voxels(matrix == 0, facecolors='r', edgecolor='k', alpha=0.5)
-    ax.voxels(matrix == 1, facecolors='g', edgecolor='k', alpha=0.5)
-    ax.voxels(matrix == 2, facecolors='b', edgecolor='k', alpha=0.5)
-
-# 保存每个角度的图像
-for angle in angles:
-    ax.clear()
-    plot_matrix(ax, matrix, colors)
-    ax.view_init(30, angle)
-    plt.draw()
-    plt.pause(.1)
-    break
-
-plt.show()
+# 显示图像
+mlab.show()
